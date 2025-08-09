164ca3930874565bd083d690468ce1356642a6ac
omzkiii <geogeomarsantos@gmail.com>
2025-08-09 21:31:39 +0800

    frontend: fetch from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,18 @@
 import { useState } from "react";
+import axios from "axios";
+const url = import.meta.env.VITE_API_BASE_URL;
 import "./App.css";
 
+function fetchMessage(): Promise<string> {
+  return axios
+    .get<string>(url)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      return `Error ${err}`;
+    });
+}
 function App() {
   const [message, setMessage] = useState("Hello World");
 
@@ -8,8 +20,9 @@ function App() {
     <>
       {message}
       <button
-        onClick={() => {
-          setMessage("Button Clicked!");
+        onClick={async () => {
+          const data: string = await fetchMessage();
+          setMessage(data);
         }}
       >
         {" "}
