1c3263c906871441d5562cf77e81ed2c45d73a7d
omzkiii <geogeomarsantos@gmail.com>
2025-08-13 00:37:13 +0800

    backend: add /signup endpoint

--- backend/main.go
+++ backend/main.go
@@ -1,14 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/rs/cors"
 )
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello")
+	fmt.Fprintln(w, "hi")
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	type signupForm struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	signupData := signupForm{}
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&signupData)
+	if err != nil {
+		log.Println("Decoding error")
+	}
+	fmt.Println(signupData)
+	w.Header().Add("Content-type", "text/html; charset=utf-8")
+	w.WriteHeader(200)
+	w.Write([]byte("FORM RECIEVED"))
 }
 
 func backendStateCheck(w http.ResponseWriter, r *http.Request) {
@@ -23,12 +42,16 @@ func main() {
 	http.HandleFunc("/", indexHandler)
 	// Route defintion End
 
+	http.HandleFunc("/", indexHandler)
+
 	handler := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:5173"},
 		AllowedMethods:   []string{"GET", "POST", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type"},
 		AllowCredentials: true,
 	}).Handler(http.DefaultServeMux)
+	http.HandleFunc("POST /signup", testHandler)
+	http.HandleFunc("GET /test", indexHandler)
 
 	fmt.Println("Listening to 3000")
 	http.ListenAndServe(":3000", handler)
