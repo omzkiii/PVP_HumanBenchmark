e0eb8d2afc68c3e7d9c0cbd9c6fa9a220a9e0dfe
omzkiii <geogeomarsantos@gmail.com>
2025-08-14 22:22:48 +0800

    backend: add JWT signing and validation

--- backend/testing_scripts/login.sh
+++ backend/testing_scripts/login.sh
@@ -1,6 +1,6 @@
 post() {
-   # curl -c cookie.txt -b cookie.txt \
-   curl -d "$(printf '{"username": "%s", "password_hash": "%s"}' "$1" "$2")" \
+   curl -c cookie.txt -b cookie.txt \
+      -d "$(printf '{"username": "%s", "password_hash": "%s"}' "$1" "$2")" \
       -X POST \
       -H 'Content-Type: application/json' \
       localhost:3000/login
