f806bdb91ffaa14ad49c6a1eabcfb2639331d576
omzkiii <geogeomarsantos@gmail.com>
2025-08-16 18:26:46 +0800

    backend: quick fix
    backend quick fix

--- backend/routes/users.go
+++ backend/routes/users.go
@@ -18,15 +18,9 @@ func Users(queries *database.Queries) {
 	http.HandleFunc("POST /signup", q.signupHandler)
 	http.HandleFunc("POST /login", q.loginHandler)
 	http.Handle("GET /users", tokenMiddleware(q.getUsersHandler))
-	// cannot use q.getUsersHandler (value of type func(w http.ResponseWriter,
-	// r *http.Request)) as http.Handler value in argument to tokenMiddleware:
-	// func(w http.ResponseWriter, r *http.Request) does not implement
-	// http.Handler (missing method ServeHTTP) compiler (InvalidIfaceAssign)
-	// routes/users.go:20:43
 }
 
 // HANDLERS
-
 type db struct {
 	*database.Queries
 }
@@ -88,15 +82,6 @@ func (q *db) signupHandler(w http.ResponseWriter, r *http.Request) {
 
 func (q *db) loginHandler(w http.ResponseWriter, r *http.Request) {
 	var err error
-	// req_token, err := r.Cookie("token")
-	// if err == nil {
-	// 	fmt.Println(req_token.Value)
-	// 	req_claims, err := validateToken(req_token.Value)
-	// 	if handleError(err, w) {
-	// 		fmt.Println("INVALID TOKEN")
-	// 	}
-	// 	fmt.Println(req_claims.ExpiresAt)
-	// }
 	data := database.CreateUserParams{}
 	err = json.NewDecoder(r.Body).Decode(&data)
 	if handleError(err, w) {
