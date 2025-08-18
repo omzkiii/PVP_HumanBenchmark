30c7b0ef26b564634ce2653533e6a0a2dada5df2
omzkiii <geogeomarsantos@gmail.com>
2025-08-19 01:33:35 +0800

    frontend: add redirect to index if auth failed

--- frontend/src/API/AuthHelper.tsx
+++ frontend/src/API/AuthHelper.tsx
@@ -1,10 +1,6 @@
 import axios, { HttpStatusCode, type AxiosResponse } from "axios";
-import React, {
-  createContext,
-  useEffect,
-  useState,
-  type ReactNode,
-} from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { useNavigate, Outlet } from "react-router-dom";
 const API = import.meta.env.VITE_API_BASE_URL;
 
 type AuthContextType = [boolean, React.Dispatch<React.SetStateAction<boolean>>];
@@ -12,25 +8,39 @@ type AuthContextType = [boolean, React.Dispatch<React.SetStateAction<boolean>>];
 export const IsAuthorized = createContext<AuthContextType | undefined>(
   undefined,
 );
-function AuthHelper({ children }: { children: ReactNode }) {
+function AuthHelper() {
   const [authStatus, setAuthStatus] = useState(false);
+  const navigate = useNavigate();
   useEffect(() => {
+    console.log("AUTH CHECK");
     const connect = async () => {
-      const auth = await axios
-        .get(API + "/auth", { withCredentials: true })
-        .then((res: AxiosResponse) => {
-          return res.status;
-        });
-      if (auth === HttpStatusCode.Ok) {
-        setAuthStatus(true);
-      } else {
+      try {
+        const auth = await axios
+          .get(API + "/auth", { withCredentials: true })
+          .then((res: AxiosResponse) => {
+            return res.status;
+          });
+
+        if (auth === HttpStatusCode.Ok) {
+          console.log("AUTH CHECK PASSED");
+          setAuthStatus(true);
+        } else {
+          console.log("AUTH CHECK FAILED");
+          setAuthStatus(false);
+          navigate("/");
+        }
+      } catch (error) {
+        console.log("AUTH CHECK FAILED");
         setAuthStatus(false);
+        navigate("/");
       }
     };
     connect();
   }, []);
   return (
-    <IsAuthorized value={[authStatus, setAuthStatus]}>{children}</IsAuthorized>
+    <IsAuthorized value={[authStatus, setAuthStatus]}>
+      <Outlet />
+    </IsAuthorized>
   );
 }
 
--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -33,7 +33,6 @@ export default function Navigation({
   return (
     <>
       <div className="Nav">
-        {String(isAuthorized)}
         <ul>
           <li>
             <Link to="/battles">BATTLES</Link>
